cbfc6d522d2e512bb30ba5a731e1af237a100e6a
almarat <arnualmarat@gmail.com>
2024-02-12 14:21:17 +0300

    all

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,20 +1,12 @@
+#include "heap.h"
 #include <stdlib.h>
 
-typedef struct node {
-    void *data;
-} node_t;
 
-typedef struct heap {
-    node_t *root;
-    size_t size;
-    int (*data_cmp)(void *, void *);
-} heap_t;
-
-heap_t *heap_create(int (*data_cmp)(void *, void *)) {
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
     heap_t *new_heap = malloc(sizeof(heap_t));
-
-    if (new_heap == NULL) {
-       
+    if (new_heap == NULL)
+    {
         return NULL;
     }
 
@@ -23,20 +15,4 @@ heap_t *heap_create(int (*data_cmp)(void *, void *)) {
     new_heap->data_cmp = data_cmp;
 
     return new_heap;
-}
-
-int int_cmp(void *a, void *b) {
-    return (*(int *)a - *(int *)b);
-}
-
-int main() {
-    heap_t *my_heap = heap_create(int_cmp);
-
-    if (my_heap == NULL) {
-        return EXIT_FAILURE;
-    }
-
-    free(my_heap);
-
-    return EXIT_SUCCESS;
 }
