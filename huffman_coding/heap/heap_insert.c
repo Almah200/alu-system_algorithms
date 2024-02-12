4bba3b40b7d33ca7b0d0e23c3d95615b8982629b
almarat <arnualmarat@gmail.com>
2024-02-13 00:09:23 +0300

    two

--- huffman_coding/heap/heap_insert.c
+++ huffman_coding/heap/heap_insert.c
@@ -1,17 +1,16 @@
 #include "heap.h"
-#include <stdlib.h>
 
 /**
- * heap_insert - Inserts a value in a Min Binary Heap
- * @heap: Pointer to the heap in which the node has to be inserted
- * @data: Pointer containing the data to store in the new node
+ * heap_insert - Inserts a value into a Min Binary Heap.
+ * @heap: Pointer to the heap in which the node has to be inserted.
+ * @data: Pointer containing the data to store in the new node.
  *
- * Return: Pointer to the created node containing data, or NULL if it fails
+ * Return: A pointer to the created binary_tree_node_t structure,
+ *         or NULL if it fails.
  */
 binary_tree_node_t *heap_insert(heap_t *heap, void *data)
 {
     binary_tree_node_t *new_node, *parent;
-    size_t index;
 
     if (!heap || !data)
         return NULL;
@@ -20,39 +19,45 @@ binary_tree_node_t *heap_insert(heap_t *heap, void *data)
     if (!new_node)
         return NULL;
 
+    heap->size++;
+
     if (!heap->root)
     {
         heap->root = new_node;
-        heap->size++;
         return new_node;
     }
 
-    index = (heap->size + 1) / 2;
     parent = heap->root;
-
-    while (index > 1)
+    while (parent->left)
     {
-        if (index & 1)
+        if (!parent->right || heap->data_cmp(parent->left->data, parent->right->data) <= 0)
+        {
             parent = parent->left;
+        }
         else
+        {
             parent = parent->right;
-
-        index /= 2;
+        }
     }
 
-    if (!(heap->size % 2))
-        parent->left = new_node;
-    else
-        parent->right = new_node;
-
     new_node->parent = parent;
-
-    heap->size++;
-
-    /* Perform heapify-up */
-    while (new_node->parent && heap->data_cmp(new_node->parent->data, new_node->data) > 0)
+    if (!parent->left)
     {
-        swap_nodes(new_node->parent, new_node);
+        parent->left = new_node;
+    }
+    else
+    {
+        parent->right = new_node;
+    }
+
+    /* Heapify Up */
+    while (new_node->parent && heap->data_cmp(new_node->data, new_node->parent->data) < 0)
+    {
+        /* Swap data between current node and parent */
+        void *temp = new_node->data;
+        new_node->data = new_node->parent->data;
+        new_node->parent->data = temp;
+
         new_node = new_node->parent;
     }
 
