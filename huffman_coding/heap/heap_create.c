0bcbe7be4d2d75c7b4b74ff6f9eea8a4e6aad554
almarat <arnualmarat@gmail.com>
2024-02-12 23:08:20 +0300

    zero

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,17 +1,25 @@
 #include "heap.h"
 #include <stdlib.h>
 
+/**
+ * @heap_t function to Create a new heap.
+ *
+ * This function initializes a new heap structure and returns a pointer to it.
+ *
+ * @param data_cmp A pointer to a comparison function.
+ * @return A pointer to the created heap_t structure, or NULL if it fails.
+ */
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
-    heap_t *new_heap = malloc(sizeof(heap_t));
+	heap_t *new_heap = malloc(sizeof(heap_t));
     if (new_heap == NULL)
-    {
-        return NULL;
-    }
-    
-    new_heap->root = NULL;
-    new_heap->size = 0;
-    new_heap->data_cmp = data_cmp;
-    
-    return new_heap;
+	{
+		return (NULL);
+	}
+
+	new_heap->root = NULL;
+	new_heap->size = 0;
+	new_heap->data_cmp = data_cmp;
+
+	return (new_heap);
 }
