5907308c364b60ed03dd0fcf3cf6c11ccee93476
almarat <arnualmarat@gmail.com>
2024-02-12 14:37:08 +0300

    all

--- huffman_coding/heap/heap.h
+++ huffman_coding/heap/heap.h
@@ -3,18 +3,33 @@
 
 #include <stdlib.h>
 
+/**
+ * @struct node
+ * Represents a node in the heap.
+ */
 typedef struct node {
     void *data;
-
 } node_t;
 
+/**
+ * @struct heap
+ * Represents a heap data structure.
+ */
 typedef struct heap {
     node_t *root;
     size_t size;
-    int (*data_cmp)(void *, void *)
+    int (*data_cmp)(void *, void *);
 
 } heap_t;
 
+/**
+ * @brief Creates a new heap.
+ *
+ * This function initializes a new heap structure and returns a pointer to it.
+ *
+ * @param data_cmp A pointer to a comparison function.
+ * @return A pointer to the created heap_t structure, or NULL if it fails.
+ */
 heap_t *heap_create(int (*data_cmp)(void *, void *));
 
 #endif /* HEAP_H */
