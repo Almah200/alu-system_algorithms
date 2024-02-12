da40e379e159dca9576c540a2c97ba65092cf7dd
almarat <arnualmarat@gmail.com>
2024-02-12 14:34:41 +0300

    all

--- huffman_coding/heap/heap.h
+++ huffman_coding/heap/heap.h
@@ -5,17 +5,16 @@
 
 typedef struct node {
     void *data;
-    // Add other members if needed
+
 } node_t;
 
 typedef struct heap {
     node_t *root;
     size_t size;
-    int (*data_cmp)(void *, void *);
-    // Add other members if needed
+    int (*data_cmp)(void *, void *)
+
 } heap_t;
 
 heap_t *heap_create(int (*data_cmp)(void *, void *));
-// Add other function prototypes if needed
 
 #endif /* HEAP_H */
--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,15 +1,14 @@
 #include "heap.h"
 
 heap_t *heap_create(int (*data_cmp)(void *, void *)) {
-    // Allocate memory for the heap structure
-    heap_t *new_heap = (heap_t *)malloc(sizeof(heap_t));
+
+    heap_t *new_heap = malloc(sizeof(heap_t));
 
     if (new_heap == NULL) {
-        // Memory allocation failed
+
         return NULL;
     }
 
-    // Initialize heap members
     new_heap->root = NULL;
     new_heap->size = 0;
     new_heap->data_cmp = data_cmp;
