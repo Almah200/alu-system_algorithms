e6fe3c785f92ef7abc334543a20cd2d7779cb895
almarat <arnualmarat@gmail.com>
2024-02-12 14:07:54 +0300

    all

--- huffman_coding/README.md
+++
@@ -1,1 +0,0 @@
-readme for huffman coding project
--- huffman_coding/heap/heap.h
+++ huffman_coding/heap/heap.h
@@ -1,9 +1,6 @@
 #ifndef _HEAP_H_
 #define _HEAP_H_
 
-#include <stddef.h>
-
-
 /**
  * struct binary_tree_node_s - Binary tree node data structure
  *
@@ -14,27 +11,10 @@
  */
 typedef struct binary_tree_node_s
 {
-    void *data;
-    struct binary_tree_node_s *left;
-    struct binary_tree_node_s *right;
-    struct binary_tree_node_s *parent;
+	void *data;
+	struct binary_tree_node_s *left;
+	struct binary_tree_node_s *right;
+	struct binary_tree_node_s *parent;
 } binary_tree_node_t;
 
-/**
- * struct heap_s - Binary heap data structure
- *
- * @root: Pointer to the root node
- * @size: Size of the heap
- * @data_cmp: Pointer to the function to compare nodes' data
- */
-typedef struct heap_s
-{
-    binary_tree_node_t *root;
-    size_t size;
-    int (*data_cmp)(void *, void *);
-} heap_t;
-
-/* Function to compare integers */
-int int_cmp(void *a, void *b);
-
-#endif
+#endif /* _HEAP_H_ */
