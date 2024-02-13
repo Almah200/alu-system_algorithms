bed1a90cc6b82cdd4b362a6b40776a9807d24421
almarat <arnualmarat@gmail.com>
2024-02-13 23:06:57 +0300

    header file

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -9,11 +9,12 @@
  * @data: The character
  * @freq: The associated frequency
  */
-typedef struct symbol_s
+typedef struct symbol
 {
 char data;
 size_t freq;
 } symbol_t;
+
 /*Trotypes*/
 symbol_t *symbol_create(char data, size_t freq);
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
