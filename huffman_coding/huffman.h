ba14848bf98718706b2f0f8f18a4104a9551da29
almarat <arnualmarat@gmail.com>
2024-02-13 22:37:13 +0300

    huffman

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -1,6 +1,7 @@
 #ifndef _HUFFMAN_H_
 #define _HUFFMAN_H_
 #include <stddef.h>
+#include "heap/heap.h"
 
 /**
  * struct symbol_s - Stores a char and its associated frequency
@@ -15,4 +16,5 @@ size_t freq;
 } symbol_t;
 /*Trotypes*/
 symbol_t *symbol_create(char data, size_t freq);
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 #endif
