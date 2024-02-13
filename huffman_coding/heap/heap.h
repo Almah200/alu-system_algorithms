3db2e8e762bd05f9fdb8f44708d95371ce0800f6
almarat <arnualmarat@gmail.com>
2024-02-13 15:37:24 +0300

    heap.h

--- huffman_coding/heap/heap.h
+++ huffman_coding/heap/heap.h
@@ -3,6 +3,7 @@
 #define _HEAP_H_
 
 #include <stdlib.h>
+
 /**
  * struct binary_tree_node_s - Binary tree node data structure
  *
@@ -40,7 +41,7 @@ binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
 binary_tree_node_t *heap_insert(heap_t *heap, void *data);
 void *heap_extract(heap_t *heap);
 void heap_delete(heap_t *heap, void (*free_data)(void *));
-/*symbol_t *symbol_create(char data, size_t freq); */
+/* symbol_t *symbol_create(char data, size_t freq); */
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 int huffman_extract_and_insert(heap_t *priority_queue);
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
