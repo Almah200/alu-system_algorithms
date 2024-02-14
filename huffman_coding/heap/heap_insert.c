fc052ba8050a752fb7505ba9ac77e1c3539a281f
almarat <arnualmarat@gmail.com>
2024-02-14 16:29:56 +0300

    all

--- huffman_coding/heap/heap_insert.c
+++ huffman_coding/heap/heap_insert.c
@@ -11,57 +11,58 @@
  */
 binary_tree_node_t *heap_insert(heap_t *heap, void *data)
 {
-    binary_tree_node_t *new_node, *parent;
-    
-    if (!heap || !data)
-    return (NULL);
-    
-    new_node = binary_tree_node(NULL, data);
-    if (!new_node)
-    
-    return (NULL);
-    
-    heap->size++;
-    
-    if (!heap->root)
-    {
-        heap->root = new_node;
-        return (new_node);
-    }
-    
-    parent = heap->root;
-    while (parent->left)
-    {
-        if (!parent->right || heap->data_cmp(parent->left->data, parent->right->data) <= 0)
-        {
-            parent = parent->left;
-        }
-        else
-        {
-            parent = parent->right;
-        }
-    }
+binary_tree_node_t *new_node, *parent;
 
-    new_node->parent = parent;
-    if (!parent->left)
-    {
-        parent->left = new_node;
-    }
-    else
-    {
-        parent->right = new_node;
-    }
+if (!heap || !data)
+return (NULL);
 
-    /* Heapify Up */
-    while (new_node->parent && heap->data_cmp(new_node->data, new_node->parent->data) < 0)
-    {
-        /* Swap data between current node and parent */
-        void *temp = new_node->data;
-        new_node->data = new_node->parent->data;
-        new_node->parent->data = temp;
+new_node = binary_tree_node(NULL, data);
+if (!new_node)
 
-        new_node = new_node->parent;
-    }
+return (NULL);
+  
+heap->size++;
 
-    return (new_node);
+if (!heap->root)
+{
+heap->root = new_node;
+return (new_node);
 }
+
+parent = heap->root;
+while (parent->left)
+{
+if (!parent->right || heap->data_cmp(parent->left->data, parent->right->data) <= 0)
+{
+parent = parent->left;
+}
+else
+{
+parent = parent->right;
+}
+}
+
+new_node->parent = parent;
+if (!parent->left)
+{
+parent->left = new_node;
+}
+else
+{
+parent->right = new_node;
+}
+
+/* Heapify Up */
+while (new_node->parent && heap->data_cmp(new_node->data, new_node->parent->data) < 0)
+{
+/* Swap data between current node and parent */
+void *temp = new_node->data;
+new_node->data = new_node->parent->data;
+new_node->parent->data = temp;
+
+new_node = new_node->parent;
+}
+
+return (new_node);
+}
+
